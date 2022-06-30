3d652305f750ca991c5ab341c20a5110feab9232
Grasiela Gomes <produtoradama@gmail.com>
2022-06-30 18:01:05 -0300

    refactor: change ui if list has just one item

--- src/components/headers/TabsHeader.tsx
+++ src/components/headers/TabsHeader.tsx
@@ -1,29 +1,36 @@
-import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
 import { TabsHeaderI } from '../../interfaces/headers/TabsHeader.interface'
 
 export const TabsHeader = ({ tabList }: TabsHeaderI) => {
+  const oneItem = tabList.length === 1
   return (
     <Tabs variant="unstyled">
-      <TabList px={4} bg="white">
-        {tabList?.map(({ label }) => (
-          <Tab
-            key={label}
-            textTransform="uppercase"
-            fontWeight="medium"
-            fontSize="sm"
-            fontFamily="primary"
-            color="font.linkHover"
-            _hover={{ cursor: 'pointer' }}
-            _focus={{ outline: 'none' }}
-            _selected={{
-              color: 'font.linkSelected',
-              borderBottom: '3px solid',
-            }}
-          >
-            {label}
-          </Tab>
-        ))}
-      </TabList>
+      <Box bg="container.neutralBlue">
+        <TabList
+          px={oneItem ? '0' : '4'}
+          bg="white"
+          w={oneItem ? 'fit-content' : 'full'}
+        >
+          {tabList?.map(({ label }) => (
+            <Tab
+              key={label}
+              textTransform="uppercase"
+              fontWeight="medium"
+              fontSize="sm"
+              fontFamily="primary"
+              color="font.linkHover"
+              _hover={{ cursor: 'pointer' }}
+              _focus={{ outline: 'none' }}
+              _selected={{
+                color: 'font.linkSelected',
+                borderBottom: '3px solid',
+              }}
+            >
+              {label}
+            </Tab>
+          ))}
+        </TabList>
+      </Box>
       <TabPanels>
         {tabList?.map(({ label, component }) => (
           <TabPanel p="base" key={label}>
