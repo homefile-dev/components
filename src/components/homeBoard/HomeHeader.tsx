7606f2bddcc5f11694d2854145c11459614b89a8
Grasiela Gomes <produtoradama@gmail.com>
2022-07-25 15:14:50 -0300

    refactor: add new project button

--- src/components/homeBoard/HomeHeader.tsx
+++ src/components/homeBoard/HomeHeader.tsx
@@ -1,24 +1,41 @@
 import { Text, Flex, Box } from '@chakra-ui/react'
 import { t } from 'i18next'
-import useWindowDimensions from '../../hooks/useWindowDimensions'
 import { HomeHeaderI } from '../../interfaces/homeBoard/HomeHeader.interface'
-import BackCircleButton from '../buttons/BackCircleButton'
+import { BackCircleButton } from '../buttons/BackCircleButton'
+import { LeftButtonAnimated } from '../buttons/LeftButtonAnimated'
+import useWindowDimensions from '../../hooks/useWindowDimensions'
 
-export const HomeHeader = ({ handleClick, homeName }: HomeHeaderI) => {
+export const HomeHeader = ({
+  handleAddNewProject,
+  handleClick,
+  homeName,
+}: HomeHeaderI) => {
   const { width } = useWindowDimensions()
   const isSmallMobile = width < 500
   return (
-    <Flex align="center" gap="1">
-      <BackCircleButton handleClick={handleClick} />
-      <Box
-        borderLeft="solid"
-        borderWidth="1px"
-        borderColor="divider"
-        h="1.75rem"
-        mx="4"
+    <Flex align="center" justify="space-between">
+      <Flex align="center" gap="1">
+        <BackCircleButton handleClick={handleClick} />
+        <Box
+          borderLeft="solid"
+          borderWidth="1px"
+          borderColor="divider"
+          h="1.75rem"
+          mx="4"
+        />
+        {!isSmallMobile && (
+          <Text variant="title">{t('myHomes.board.title')}</Text>
+        )}
+        <Text variant="titleBolder">{`${homeName} ${t(
+          'myHomes.addHome'
+        )}`}</Text>
+      </Flex>
+      <LeftButtonAnimated
+        bgColor="button.green"
+        handleClick={handleAddNewProject}
+        height="full"
+        label={t('myHomes.addProject')}
       />
-      {!isSmallMobile && <Text variant="title">{t('myHomes.board.title')}</Text>}
-      <Text variant="titleBolder">{`${homeName} ${t('myHomes.addHome')}`}</Text>
     </Flex>
   )
 }
